4b90927da8a1500af319f9a4f4f8fe92ad168b06
sarah <github@stoffels.it>
2020-03-23 00:59:20 +0100

    removed unused imports

--- models.py
+++ models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phone_field import PhoneField
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Person(AbstractUser):
